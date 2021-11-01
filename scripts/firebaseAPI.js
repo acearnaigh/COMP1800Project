ae62d8c719ebb42b65dfb6ccd41ab13717d5ab1c
acearnaigh <90723095+acearnaigh@users.noreply.github.com>
2021-10-31 18:34:19 -0700

    Add files via upload
    Added a scripts folder and a file for the implementation of firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDCzZQUkQh1ak8bKDDbKeL6DkjIhSTCA9M",
+  authDomain: "comp-1800-project-bby-18.firebaseapp.com",
+  projectId: "comp-1800-project-bby-18",
+  storageBucket: "comp-1800-project-bby-18.appspot.com",
+  messagingSenderId: "416108549162",
+  appId: "1:416108549162:web:b748773cedf38456725f53"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+
