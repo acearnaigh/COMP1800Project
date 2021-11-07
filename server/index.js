e3c202dec79b526068691666c9e98ba22b35ce2d
Leerang(Naomi) Byun <naomibyun7@gmail.com>
2021-11-06 23:39:29 -0700

    create API for creating posts

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { collection, addDoc } = require("firebase/firestore");
+
+const db = require("./firebase");
 const app = express();
+
+app.use(express.json());
+
+app.post("/posts", async (req, res) => {
+  const data = req.body;
+  const collectionRef = collection(db, "posts");
+  await addDoc(collectionRef, data);
+  res.status(200).send();
+});
 
 let port = 8000;
 
