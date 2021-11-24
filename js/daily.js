7b9496dd556d6df071f3edacaa8aa47361bab430
Manjot Singh Randhawa <msrandhawa9957@gmail.com>
2021-11-23 17:03:15 -0800

    Daily view is done. ToDo and daily events are linked, filtered and ordered. The schedule collection was restructutred to not go into sub-collections.

--- js/daily.js
+++ js/daily.js
@@ -1,12 +1,256 @@
-db.collection('testSchedule')
-    .doc('Monday')
-    .listCollections()
-    .then((subCollections) => {
-        subCollections.forEach((subCollection) => {
-            subCollection.get().then((array) => {
-                array.docs.forEach((doc) => {
-                    console.log(doc.data())
-                })
-            })
+// db.collection('testSchedule')
+//     .doc('Monday')
+//     .listCollections()
+//     .then((subCollections) => {
+//         subCollections.forEach((subCollection) => {
+//             subCollection.get().then((array) => {
+//                 array.docs.forEach((doc) => {
+//                     console.log(doc.data())
+//                 })
+//             })
+//         })
+//     })
+const whatDate = new Date();
+const weekday = new Array(7);
+weekday[0] = 'Sunday';
+weekday[1] = 'Monday';
+weekday[2] = 'Tuesday';
+weekday[3] = 'Wednesday';
+weekday[4] = 'Thursday';
+weekday[5] = 'Friday';
+weekday[6] = 'Saturday';
+
+var timeOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+};
+
+let today = weekday[whatDate.getDay()];
+console.log(today);
+window.addEventListener('load', function () {
+    document.getElementById('whatIsToday').textContent = today;
+    //disabling the button add_post after you're on the right page
+    let daily = document.getElementById('daily');
+    daily.getElementsByTagName('a')[0].removeAttribute('href');
+    daily.getElementsByClassName('foot_icons')[0].style.color = 'black';
+    daily.getElementsByClassName('centering_icon_text')[0].style.color =
+        'black';
+    //------------------------------------------------------------------------
+    let dailyContainer = document.getElementById('dailyContainer');
+    let falseContainer = document.getElementById('falseDaily');
+    let trueContainer = document.getElementById('trueDaily');
+
+    db.collection('dayCRN')
+        .doc(today)
+        .get()
+        .then((docs) => {
+            const classList = docs.data().classList;
+            for (const key in classList) {
+                classList[key];
+                db.collection(classList[key])
+                    .doc(today)
+                    .get()
+                    .then((docs) => {
+                        let dailyTemplate =
+                            dailyContainer.content.cloneNode(true);
+                        dailyTemplate.querySelector('.classCode').textContent =
+                            docs.data().courseID;
+                        dailyTemplate.querySelector('.classType').textContent =
+                            docs.data().courseType;
+                        dailyTemplate.querySelector('.classRoom').textContent =
+                            docs.data().courseRoom;
+                        dailyTemplate.querySelector('.classStart').textContent =
+                            new Intl.DateTimeFormat(
+                                'en-US',
+                                timeOptions
+                            ).format(docs.data().courseStartTime.toDate());
+                        dailyTemplate.querySelector('.classEnd').textContent =
+                            new Intl.DateTimeFormat(
+                                'en-US',
+                                timeOptions
+                            ).format(docs.data().courseEndTime.toDate());
+                        dailyTemplate.querySelector('.classContainer').id =
+                            this.localStorage.getItem('userID') +
+                            classList[key];
+                        falseContainer.appendChild(dailyTemplate);
+                    });
+            }
         })
-    })
+        .then(function () {
+            let taskContainer = document.getElementById('dailyTaskContainer');
+            db.collection('tasks')
+                .doc(this.localStorage.getItem('userID'))
+                .get()
+                .then((taskDoc) => {
+                    const taskData = taskDoc.data();
+                    for (const key in taskData) {
+                        const value = taskData[key];
+
+                        var milliseconds = key
+                            .split(this.localStorage.getItem('userID'))
+                            .pop();
+                        let taskDate = new Date(
+                            parseInt(milliseconds, 10)
+                        ).setHours(0, 0, 0, 0);
+                        let justDailyDate = whatDate.setHours(0, 0, 0, 0);
+                        console.log(milliseconds);
+                        if (taskDate.valueOf() === justDailyDate.valueOf()) {
+                            console.log(taskDate);
+                            console.log(justDailyDate);
+                            let taskTemplate =
+                                taskContainer.content.cloneNode(true);
+                            taskTemplate.querySelector('.readToDo').innerHTML =
+                                value;
+                            taskTemplate.querySelector(
+                                '.readTime'
+                            ).textContent = new Intl.DateTimeFormat(
+                                'en-US',
+                                timeOptions
+                            ).format(milliseconds);
+                            taskTemplate.querySelector('.todoContainer').id =
+                                localStorage.getItem('userID') + milliseconds;
+                            falseContainer.appendChild(taskTemplate);
+                        }
+                    }
+                })
+                .then(function () {
+                    let idArr = [];
+                    let elementWithId = document
+                        .getElementById('falseDaily')
+                        .querySelectorAll('*[id]:not([id="unfold"])');
+                    Array.prototype.forEach.call(
+                        elementWithId,
+                        function (el, i) {
+                            // "el" is your element
+                            console.log(el.id); // log the ID
+                            idArr.push(el.id);
+                        }
+                    );
+
+                    console.log(idArr);
+                    idArr.sort(function compareDatesbyID(id1, id2) {
+                        let dateID1 = new Date(
+                            'Sun Jan 1 2021 ' +
+                                document
+                                    .getElementById(id1)
+                                    .querySelector('.readTime').textContent
+                        );
+                        let dateID2 = new Date(
+                            'Sun Jan 1 2021 ' +
+                                document
+                                    .getElementById(id2)
+                                    .querySelector('.readTime').textContent
+                        );
+
+                        if (dateID1.valueOf() < dateID2.valueOf()) {
+                            return -1;
+                        }
+                        if (dateID1.valueOf() > dateID2.valueOf()) {
+                            return 1;
+                        }
+                        if (dateID1.valueOf() === dateID2.valueOf()) {
+                            return 0;
+                        }
+                    });
+                    console.log(idArr);
+                    Array.prototype.forEach.call(idArr, function (elID, i) {
+                        $('#' + elID).appendTo('#trueDaily');
+                    });
+                })
+                .then(function () {
+                    moreInfoListen = document.querySelectorAll('#unfold');
+                    let dailyClassInfo =
+                        document.getElementById('dailyClassInfo');
+
+                    for (let x = 0; x < moreInfoListen.length; x++) {
+                        moreInfoListen[x].addEventListener(
+                            'click',
+                            function () {
+                                let collectionName =
+                                    this.parentNode.parentNode.id
+                                        .split(localStorage.getItem('userID'))
+                                        .pop();
+                                console.log(collectionName);
+                                db.collection(collectionName)
+                                    .doc(today)
+                                    .get()
+                                    .then((docs) => {
+                                        let dailyInfo =
+                                            dailyClassInfo.content.cloneNode(
+                                                true
+                                            );
+                                        let infoID =
+                                            'Info' +
+                                            localStorage.getItem('userID') +
+                                            collectionName;
+                                        let checkID =
+                                            document.getElementById(infoID);
+                                        if (checkID != null) {
+                                            $('#' + infoID).hide('slow');
+                                        } else {
+                                            dailyInfo.querySelector(
+                                                '.classInfo'
+                                            ).id = infoID;
+                                            dailyInfo.querySelector(
+                                                '.classInfo'
+                                            ).id = infoID;
+                                            dailyInfo.querySelector(
+                                                '.className'
+                                            ).textContent =
+                                                'Name: ' +
+                                                docs.data().courseName;
+                                            dailyInfo.querySelector(
+                                                '.classDelivery'
+                                            ).textContent =
+                                                'Delivery Method: ' +
+                                                docs.data().courseDelivery;
+                                            dailyInfo.querySelector(
+                                                '.classInstructor'
+                                            ).textContent =
+                                                'Instrctor: ' +
+                                                docs.data().courseInstructor;
+                                            dailyInfo.querySelector(
+                                                '.instructorEmail'
+                                            ).textContent =
+                                                'Instructor Email: ' +
+                                                docs.data().instructorEmail;
+                                            if (
+                                                docs.data().courseLink != null
+                                            ) {
+                                                dailyInfo
+                                                    .querySelector('.classLink')
+                                                    .getElementsByTagName(
+                                                        'a'
+                                                    )[0]
+                                                    .setAttribute(
+                                                        'href',
+                                                        docs.data().courseLink
+                                                    );
+                                                dailyInfo
+                                                    .querySelector('.classLink')
+                                                    .getElementsByTagName(
+                                                        'a'
+                                                    )[0]
+                                                    .setAttribute(
+                                                        'target',
+                                                        '_blank'
+                                                    );
+                                            }
+                                            $(dailyInfo).insertAfter(
+                                                this.parentNode.parentNode
+                                            );
+
+                                            $('#' + infoID).show('slow');
+
+                                            document.getElementById(
+                                                infoID
+                                            ).style.display = 'grid';
+                                        }
+                                    });
+                            }
+                        );
+                    }
+                });
+        });
+});
---
+++ js/dailyCollection.js
@@ -0,0 +1,356 @@
+db.collection('CRN30388')
+    .doc('Monday')
+    .set({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SE12 321',
+        courseStartTime: new Date(
+            'Mon Nov 15 2021 08:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Mon Nov 15 2021 10:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Ife Agboola',
+        instructorEmail: 'ife_agboola@bcit.ca',
+    });
+
+db.collection('CRN30388')
+    .doc('Wednesday')
+    .update({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SE12 320',
+        courseStartTime: new Date(
+            'Wed Nov 17 2021 12:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Wed Nov 17 2021 13:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Ife Agboola',
+        instructorEmail: 'ife_agboola@bcit.ca',
+    });
+
+db.collection('CRN30384')
+    .doc('Monday')
+    .update({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SW05 1840',
+        courseStartTime: new Date(
+            'Mon Nov 15 2021 10:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Mon Nov 15 2021 11:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/97990532046?pwd=LzlpOGhJbnNEcCt6NW9la2hiVXlydz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Bruce D. Link',
+        instructorEmail: 'Bruce_Link@bcit.ca',
+    });
+
+db.collection('CRN30384')
+    .doc('Tuesday')
+    .update({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SW05 1850',
+        courseStartTime: new Date(
+            'Tue Nov 16 2021 15:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Tue Nov 16 2021 16:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/97990532046?pwd=LzlpOGhJbnNEcCt6NW9la2hiVXlydz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Bruce D. Link',
+        instructorEmail: 'Bruce_Link@bcit.ca',
+    });
+
+db.collection('CRN30384')
+    .doc('Wednesday')
+    .update({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SW05 1850',
+        courseStartTime: new Date(
+            'Wed Nov 17 2021 13:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Wed Nov 17 2021 14:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/97990532046?pwd=LzlpOGhJbnNEcCt6NW9la2hiVXlydz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Bruce D. Link',
+        instructorEmail: 'Bruce_Link@bcit.ca',
+    });
+
+db.collection('CRN31002')
+    .doc('Monday')
+    .update({
+        courseID: 'COMP 1100',
+        courseName: 'CST Program Fundamentals',
+        courseRoom: 'SW05 1840',
+        courseStartTime: new Date(
+            'Mon Nov 15 2021 11:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Mon Nov 15 2021 12:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/63854394574?pwd=OXVaT3VFbUNtWHBXcGVWZUpTcDZvdz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Donna E. Turner',
+        instructorEmail: 'donna_turner@bcit.ca',
+    });
+
+db.collection('CRN46873')
+    .doc('Tuesday')
+    .update({
+        courseID: 'COMP 1800',
+        courseName: 'Projects 1',
+        courseRoom: 'SW03 1710',
+        courseStartTime: new Date(
+            'Tue Nov 16 2021 10:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Tue Nov 16 2021 12:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/69582366846?pwd=Vk8xNWpNSFc3V1FDNk82d3IwOEdxdz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Carly Y. Orr',
+        instructorEmail: 'Carly_Wong_Orr@bcit.ca',
+    });
+
+db.collection('CRN46860')
+    .doc('Tuesday')
+    .update({
+        courseID: 'COMP 1537',
+        courseName: 'Web Development 1',
+        courseRoom: 'SW05 1850',
+        courseStartTime: new Date(
+            'Tue Nov 16 2021 13:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Tue Nov 16 2021 15:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/66246033447?pwd=eFFhOG1ISHJKZHpsOTcrQ1hCUDExZz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Arron Ferguson',
+        instructorEmail: 'Arron_Ferguson@bcit.ca',
+    });
+
+db.collection('CRN48401')
+    .doc('Wednesday')
+    .update({
+        courseID: 'COMP 1510',
+        courseName: 'Programming Methods',
+        courseRoom: 'SE12 321',
+        courseStartTime: new Date(
+            'Wed Nov 17 2021 08:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Wed Nov 17 2021 09:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Tutorial',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Ife Agboola',
+        instructorEmail: 'ife_agboola@bcit.ca',
+    });
+
+db.collection('CRN32203')
+    .doc('Wednesday')
+    .update({
+        courseID: 'COMP 1113',
+        courseName: 'Applied Mathematics',
+        courseRoom: 'SW05 2875',
+        courseStartTime: new Date(
+            'Wed Nov 17 2021 10:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Wed Nov 17 2021 12:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Paul Rozman',
+        instructorEmail: 'Paul_Rozman@bcit.ca',
+    });
+db.collection('CRN46878')
+    .doc('Thursday')
+    .update({
+        courseID: 'COMP 1800',
+        courseName: 'Projects 1',
+        courseRoom: 'SE12 322',
+        courseStartTime: new Date(
+            'Thu Nov 18 2021 08:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Thu Nov 18 2021 10:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink:
+            'https://bcit.zoom.us/j/66573536191?pwd=Y2E5bitvU3gxdmlJNTlTWGE3Kzh2dz09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Carly Y. Orr',
+        instructorEmail: 'Carly_Wong_Orr@bcit.ca',
+    });
+
+db.collection('CRN34590')
+    .doc('Thursday')
+    .update({
+        courseID: 'COMM 1116',
+        courseName: 'Business Communications 1',
+        courseRoom: 'SE12 313',
+        courseStartTime: new Date(
+            'Thu Nov 18 2021 10:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Thu Nov 18 2021 12:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Thorsten Ewald',
+        instructorEmail: 'Thorsten_Ewald@bcit.ca',
+    });
+
+db.collection('CRN46864')
+    .doc('Thursday')
+    .update({
+        courseID: 'COMP 1537',
+        courseName: 'Web Development 1',
+        courseRoom: 'SE12 322',
+        courseStartTime: new Date(
+            'Thu Nov 18 2021 13:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Thu Nov 18 2021 15:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink:
+            'https://bcit.zoom.us/j/68055852135?pwd=Zno2cXVJWVBRTTArUStqdlFTK3BEUT09',
+        courseDelivery: 'Online',
+        courseInstructor: 'Arron Ferguson',
+        instructorEmail: 'Arron_Ferguson@bcit.ca',
+    });
+
+db.collection('CRN45304')
+    .doc('Thursday')
+    .update({
+        courseID: 'COMP 1712',
+        courseName: 'Biz Analysis and System Design',
+        courseRoom: 'SE12 322',
+        courseStartTime: new Date(
+            'Thu Nov 18 2021 15:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Thu Nov 18 2021 17:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lab',
+        courseLink: null,
+        courseDelivery: 'In-Person',
+        courseInstructor: 'Ife Agboola',
+        instructorEmail: 'ife_agboola@bcit.ca',
+    });
+
+db.collection('CRN34327')
+    .doc('Friday')
+    .update({
+        courseID: 'COMM 1116',
+        courseName: 'Business Communications 1',
+        courseRoom: 'SW05 1840',
+        courseStartTime: new Date(
+            'Fri Nov 19 2021 08:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Fri Nov 19 2021 10:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink:
+            'https://bcit.zoom.us/j/63689251214?pwd=UDlNV0cwcHZtS0xKQmxQY2ZuMEdTUT09',
+        courseDelivery: 'Online/Async',
+        courseInstructor: 'Thorsten Ewald',
+        instructorEmail: 'Thorsten_Ewald@bcit.ca',
+    });
+
+db.collection('CRN45299')
+    .doc('Friday')
+    .update({
+        courseID: 'COMP 1712',
+        courseName: 'Biz Analysis and System Design',
+        courseRoom: 'NE01 331',
+        courseStartTime: new Date(
+            'Fri Nov 19 2021 10:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Fri Nov 19 2021 12:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink: null,
+        courseDelivery: 'Online/Bongo',
+        courseInstructor: 'Maryam Khezrzadeh',
+        instructorEmail: 'mKhezrzadeh@bcit.ca',
+    });
+db.collection('CRN32200')
+    .doc('Friday')
+    .update({
+        courseID: 'COMP 1113',
+        courseName: 'Applied Mathematics',
+        courseRoom: 'SW01 1205',
+        courseStartTime: new Date(
+            'Fri Nov 19 2021 14:30:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseEndTime: new Date(
+            'Fri Nov 19 2021 16:20:00 GMT-0800 (Pacific Standard Time)'
+        ),
+        courseType: 'Lecture',
+        courseLink: null,
+        courseDelivery: 'Online/Async',
+        courseInstructor: 'Paul Rozman',
+        instructorEmail: 'Paul_Rozman@bcit.ca',
+    });
+
+db.collection('dayCRN')
+    .doc('Monday')
+    .update({
+        classList: ['CRN30388', 'CRN30384', 'CRN31002'],
+    });
+
+db.collection('dayCRN')
+    .doc('Tuesday')
+    .update({
+        classList: ['CRN30384', 'CRN46873', 'CRN46860'],
+    });
+db.collection('dayCRN')
+    .doc('Wednesday')
+    .update({
+        classList: ['CRN30388', 'CRN30384', 'CRN48401', 'CRN32203'],
+    });
+db.collection('dayCRN')
+    .doc('Thrusday')
+    .update({
+        classList: ['CRN46878', 'CRN34590', 'CRN46864', 'CRN45304'],
+    });
+db.collection('dayCRN')
+    .doc('Friday')
+    .update({
+        classList: ['CRN34327', 'CRN45299', 'CRN32200'],
+    });
